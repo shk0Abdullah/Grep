11ca904abf2a1eaaca3556c515d6035b0ee64968
Abdullah Amjad <shkabdullah268@gmail.com>
2026-08-16 23:53:01 -0700

    feat: reading terminal commands and reading exact file path

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = std::env::args().collect();
+
+    println!("{:?}", args[1]);
+
+    let file_path = args[2].clone();
+    println!("File path: {}", file_path);
+    let contents = std::fs::read_to_string(file_path)?;
+    println!("{:#?}", contents);
+
+    Ok(())
+    
+    // let file = File::open(args[1]).clone().unwrap();
+    // let reader = BufReader::new(file);
+    // for line in reader.lines() {
+    //     println!("{}", line.unwrap());
+    // }
 }
