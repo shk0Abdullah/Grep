f5a35ba3a9eee6497391403c3f37f03e621fdba9
Abdullah Amjad <shkabdullah268@gmail.com>
2026-08-16 23:57:35 -0700

    finding the lines with Big O (n) iterate over each file

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("{:?}", args[1]);
 
+    let query = args[1].clone();
     let file_path = args[2].clone();
     println!("File path: {}", file_path);
     let contents = std::fs::read_to_string(file_path)?;
-    println!("{:#?}", contents);
-
+    // println!("{:#?}", contents);
+    for line in contents.lines(){
+        if line.contains(&query){
+            println!("{:#?}", line);
+        }
+    }    
     Ok(())
-    
     // let file = File::open(args[1]).clone().unwrap();
     // let reader = BufReader::new(file);
     // for line in reader.lines() {
